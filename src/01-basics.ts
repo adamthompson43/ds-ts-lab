c9dce93cc44e27451a65c13567b927aae0bc07cc
Adam T <aovert.at@gmail.com>
2025-09-17 10:30:26 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -6,6 +6,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string
+    department: string
+    contact: {
+        email: string,
+        extension: number
+    }
+}
+
 
 const friend1 = {
   name: "Paul Fleming",
